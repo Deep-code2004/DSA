8e23fae841b750a3739c0f71d11f00e154ef6a22
Deep Khatri <deepkhatri050@gmail.com>
2025-10-17 06:08:54 -0700

    Traversal in array DS

---
+++ arrray-traversal.cpp
@@ -0,0 +1,9 @@
+# include <iostream>
+using namespace std;
+int main(){
+ int arr [6] = {1,2,3,4,5,57};
+ for(int i=0; i<=5; i++){
+    cout <<"Index: "<< i << " Value: "<<  arr[i] << endl;
+ }
+ return 0;
+} 
